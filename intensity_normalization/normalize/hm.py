major updates to ravel, hm, and ws normalization routines, added a tissue class mask script, fixed some bugs all around and added documentation

intensity_normalization/normalize/hm.py
@@ -26,6 +26,7 @@ Created on: May 01, 2018
 from __future__ import print_function, division
 
 from glob import glob
+import logging
 import os
 
 import numpy as np
@@ -36,6 +37,8 @@ from rpy2.rinterface import NULL
 from intensity_normalization.utilities import io
 
 ravel = importr('RAVEL')
+
+logger = logging.getLogger(__name__)
 
 
 def hm_normalize(img_dir, template_mask, contrast, output_dir=None, write_to_disk=True):
@@ -63,7 +66,7 @@ def hm_normalize(img_dir, template_mask, contrast, output_dir=None, write_to_dis
             normalization on MRIs of human brain with multiple sclerosis,”
             Med. Image Anal., vol. 15, no. 2, pp. 267–282, 2011.
     """
-    data = glob(os.path.join(img_dir, '*.nii*'))
+    data = sorted(glob(os.path.join(img_dir, '*.nii*')))
     input_files = StrVector(data)
     if output_dir is None:
         output_files = NULL
@@ -73,7 +76,16 @@ def hm_normalize(img_dir, template_mask, contrast, output_dir=None, write_to_dis
             _, base, ext = io.split_filename(fn)
             out_fns.append(os.path.join(output_dir, base + ext))
         output_files = StrVector(out_fns)
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+
+    # control verbosity of output when making whitestripe function call
+    if 0 < logger.getEffectiveLevel() <= logging.getLevelName('DEBUG'):
+        verbose = True
+    else:
+        verbose = False
+
     normalizedR = ravel.normalizeHM(input_files, output_files=output_files, brain_mask=template_mask,
-                                    type=contrast, writeToDisk=write_to_disk, returnMatrix=True, verbose=False)
+                                    type=contrast, writeToDisk=write_to_disk, returnMatrix=True, verbose=verbose)
     normalized = np.array(normalizedR)
     return normalized

intensity_normalization/normalize/ravel.py
@@ -74,8 +74,16 @@ def ravel_normalize(img_dir, template_mask, csf_mask, contrast, output_dir=None,
         output_files = StrVector(out_fns)
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
-    normalizedR = ravel.normalizeRAVEL(input_files, control_mask=csf_mask, output_files=output_files, brain_mask=template_mask,
-                                       WhiteStripe_Type=contrast, writeToDisk=write_to_disk, returnMatrix=True, verbose=False, **kwargs)
+
+    if 0 < logger.getEffectiveLevel() <= logging.getLevelName('DEBUG'):
+        verbose = True
+    else:
+        verbose = False
+
+    normalizedR = ravel.normalizeRAVEL(input_files, control_mask=csf_mask,
+                                       output_files=output_files, brain_mask=template_mask,
+                                       WhiteStripe_Type=contrast, writeToDisk=write_to_disk,
+                                       returnMatrix=True, verbose=verbose, **kwargs)
     normalized = np.array(normalizedR)
     return normalized
 

intensity_normalization/normalize/whitestripe.py
@@ -29,7 +29,6 @@ import os
 import numpy as np
 from rpy2.robjects.vectors import StrVector, IntVector
 from rpy2.robjects.packages import importr
-from rpy2.robjects.numpy2ri import py2ri
 from rpy2.rinterface import NULL
 
 from intensity_normalization.errors import NormalizationError
@@ -67,7 +66,11 @@ def ws_normalize(img_dir, contrast, mask_dir=None, output_dir=None, write_to_dis
             techniques for magnetic resonance imaging,” NeuroImage Clin.,
             vol. 6, pp. 9–19, 2014.
     """
+
+    # grab the file names for the images of interest
     data = sorted(glob(os.path.join(img_dir, '*.nii*')))
+
+    # define and get the brain masks for the images, if defined
     if mask_dir is None:
         masks = [NULL] * len(data)
     else:
@@ -75,6 +78,8 @@ def ws_normalize(img_dir, contrast, mask_dir=None, output_dir=None, write_to_dis
         if len(data) != len(masks):
             NormalizationError('Number of images and masks must be equal, Images: {}, Masks: {}'
                                .format(len(data), len(masks)))
+
+    # define the output directory and corresponding output file names
     if output_dir is None:
         output_files = [NULL] * len(data)
     else:
@@ -83,6 +88,22 @@ def ws_normalize(img_dir, contrast, mask_dir=None, output_dir=None, write_to_dis
             _, base, ext = io.split_filename(fn)
             out_fns.append(os.path.join(output_dir, base + ext))
         output_files = StrVector(out_fns)
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+
+    # define the slices to use for whitestripe analysis if defined, otherwise use whole img
+    if slices is not None and len(slices) == 2:
+        ws_slices = IntVector(range(slices[0], slices[1]))
+    else:
+        ws_slices = NULL
+
+    # control verbosity of output when making whitestripe function call
+    if 0 < logger.getEffectiveLevel() <= logging.getLevelName('DEBUG'):
+        verbose = True
+    else:
+        verbose = False
+
+    # do whitestripe normalization and save the results
     for i, (img_fn, mask_fn, output_fn) in enumerate(zip(data, masks, output_files), 1):
         logger.info('Normalizing image: {} ({:d}/{:d})'.format(img_fn, i, len(data)))
         img = nb.check_nifti(img_fn, reorient=False, allow_array=False)
@@ -91,11 +112,12 @@ def ws_normalize(img_dir, contrast, mask_dir=None, output_dir=None, write_to_dis
         else:
             mask = nb.check_nifti(mask_fn, reorient=False, allow_array=False)
             brain = nb.mask_img(img, mask)
-        indices = ws.whitestripe(brain, type=contrast, slices=IntVector(range(slices[0],slices[1])),
-                                 verbose=False)
-        brain = ws.whitestripe_norm(brain, indices[0])
+        indices = ws.whitestripe(brain, type=contrast, slices=ws_slices, verbose=verbose)
+        img = ws.whitestripe_norm(img, indices[0])
         if write_to_disk:
             logger.info('Saving normalized image: {} ({:d}/{:d})'.format(output_fn, i, len(data)))
-            nb.write_nifti(brain, output_fn)
-    normalized = np.array(brain.slots['.Data'])
+            nb.write_nifti(img, output_fn)
+
+    # output the last normalized image (mostly for testing purposes)
+    normalized = np.array(img.slots['.Data'])
     return normalized

intensity_normalization/plot/hist.py
@@ -63,7 +63,7 @@ def all_hists(img_dir, mask_dir=None, alpha=0.4, figsize=(12,10), **kwargs):
             mask = None
         _ = hist(img, mask, ax=ax, alpha=alpha, **kwargs)
     ax.set_xlabel('Intensity')
-    ax.set_ylabel('Log Count')
+    ax.set_ylabel(r'Log$_{10}$ Count')
     ax.set_ylim((0, None))
     return ax
 
@@ -86,14 +86,14 @@ def hist(img, mask=None, ax=None, n_bins=200, log=True, alpha=0.8, **kwargs):
     """
     if ax is None:
         _, ax = plt.subplots()
-    data = img.numpy() * mask.numpy() if mask is not None else img.numpy()
+    data = img.numpy()[mask.numpy()==1] if mask is not None else img.numpy()
     hist, bin_edges = np.histogram(data.flatten(), n_bins, **kwargs)
     bins = np.diff(bin_edges)/2 + bin_edges[:-1]
     if log:
         # catch divide by zero warnings in call to log
         with warnings.catch_warnings():
             warnings.filterwarnings('ignore')
-            hist =  np.log(hist)
+            hist =  np.log10(hist)
             hist[hist == -np.inf] = 0
     ax.plot(bins, hist, alpha=alpha)
     return ax