9cfa1fa0ec5c3d780dda271b303b722254d4afeb
Jacob Reinhold <jcreinhold@gmail.com>
2018-06-12 18:41:07 -0400

    removed registration requirement of RAVEL by using CSF voxels above a certain (high) membership value determined by atropos segmentation, seems to work fine but need to conduct more extensive tests

--- intensity_normalization/normalize/ravel.py
+++ intensity_normalization/normalize/ravel.py
@@ -25,27 +25,32 @@ import numpy as np
 
 from intensity_normalization.errors import NormalizationError
 from intensity_normalization.normalize.whitestripe import whitestripe, whitestripe_norm
+from intensity_normalization.utilities import csf
 from intensity_normalization.utilities import io
-
 
 logger = logging.getLogger(__name__)
 
 
-def ravel_normalize(img_dir, template_mask, control_mask, contrast,
-                    output_dir=None, write_to_disk=False, do_whitestripe=True, k=1):
+def ravel_normalize(img_dir, mask_dir, contrast, output_dir=None, write_to_disk=False,
+                    do_whitestripe=True, b=1, membership_thresh=0.99):
     """
     Use RAVEL [1] to normalize the intensities of a set of MR images to eliminate
     unwanted technical variation in images (but, hopefully, preserve biological variation)
 
+    this is modified from [1] in that *no* registration is done, the control mask is defined
+    dynamically by finding a tissue segmentation of the brain and thresholding the membership
+    at a very high level (this seems to work well and is *much* faster)
+
     Args:
         img_dir (str): directory containing MR images to be normalized
-        template_mask (str): brain mask for template image
-        control_mask (str): path to mask of control region (e.g., CSF) for data in data_dir
+        mask_dir (str): brain masks for imgs
         contrast (str): contrast of MR images to be normalized (T1, T2, or FLAIR)
         output_dir (str): directory to save images if you do not want them saved in
             same directory as data_dir
         write_to_disk (bool): write the normalized data to disk or nah
-        kwargs: ravel keyword arguments not included here
+        do_whitestripe (bool): whitestripe normalize the images before applying RAVEL correction
+        b (int): number of unwanted factors to estimate
+        membership_thresh (float): threshold of membership for control voxels
 
     Returns:
         Z (np.ndarray): unwanted factors (used in ravel correction)
@@ -58,6 +63,7 @@ def ravel_normalize(img_dir, template_mask, control_mask, contrast,
             pp. 198–212, 2016.
     """
     img_fns = sorted(glob(os.path.join(img_dir, '*.nii*')))
+    mask_fns = sorted(glob(os.path.join(mask_dir, '*.nii*')))
 
     if output_dir is None or not write_to_disk:
         out_fns = None
@@ -72,13 +78,12 @@ def ravel_normalize(img_dir, template_mask, control_mask, contrast,
     verbose = True if logger.getEffectiveLevel() == logging.getLevelName('DEBUG') else False
 
     # get parameters necessary and setup the V array
-    V, Vc = image_matrix(img_fns, contrast, masks=template_mask,
-                         control_mask=control_mask, do_whitestripe=do_whitestripe,
-                         verbose=verbose)
+    V, Vc = image_matrix(img_fns, contrast, masks=mask_fns, do_whitestripe=do_whitestripe,
+                         verbose=verbose, return_ctrl_matrix=True, membership_thresh=membership_thresh)
 
     # estimate the unwanted factors Z
     _, _, vh = np.linalg.svd(Vc)
-    Z = vh.T[:, 0:k]
+    Z = vh.T[:, 0:b]
 
     # perform the ravel correction
     V_norm = ravel_correction(V, Z)
@@ -113,57 +118,71 @@ def ravel_correction(V, Z):
     return res
 
 
-def image_matrix(imgs, contrast, masks=None, control_mask=None,
-                 do_whitestripe=True, verbose=False):
+def image_matrix(imgs, contrast, masks=None, do_whitestripe=True, return_ctrl_matrix=False,
+                 membership_thresh=0.99, smoothness=0.25, verbose=False):
     """
     creates an matrix of images where the rows correspond the the voxels of
     each image and the columns are the images
-
-    if a control mask is supplied, then a similarly shaped matrix is also output
-    where the rows correspond to voxels defined in the control mask
 
     Args:
         imgs (list): list of paths to MR images of interest
         contrast (str): contrast of the set of imgs (e.g., T1)
         masks (list or str): list of corresponding brain masks or just one (template) mask
-        control_mask (str): image of control voxels (usually CSF mask)
         do_whitestripe (bool): do whitestripe on the images before storing in matrix or nah
+        return_ctrl_matrix (bool): return control matrix for imgs (i.e., a subset of V's rows)
+        membership_thresh (float): threshold of membership for control voxels (want this very high)
+        smoothness (float): smoothness parameter for segmentation for control voxels
         verbose (bool): pass verbosity option to whitestripe if desired
 
     Returns:
         V (np.ndarray): image matrix (rows are voxels, columns are images)
         Vc (np.ndarray): image matrix of control voxels (rows are voxels, columns are images)
-            Vc only returned if a control mask and one template mask is provided
+            Vc only returned if return_ctrl_matrix is True
     """
     img_shape = io.open_nii(imgs[0]).get_data().shape
     V = np.zeros((int(np.prod(img_shape)), len(imgs)))
-    if control_mask is not None and masks is not None and isinstance(masks, str):
-        cmask = io.open_nii(control_mask)
-        mask_ = io.open_nii(masks)
-        cmask_data = cmask.get_data() * mask_.get_data()  # make sure that the template and control overlap
-        num_c_pts = int(np.sum(cmask_data.flatten()))
-        Vc = np.zeros((num_c_pts, len(imgs)))
-        masks = [masks] * len(imgs)
-    elif control_mask is not None and (masks is None or not isinstance(masks, str)):
-        raise NormalizationError('If control mask provided, then *one* template brain mask must be provided')
 
+    if return_ctrl_matrix:
+        ctrl_vox = []
+
+    if masks is None and return_ctrl_matrix:
+        raise NormalizationError('Brain masks must be provided if returning control memberships')
     if masks is None:
         masks = [None] * len(imgs)
 
     # do whitestripe on the image before applying RAVEL (if desired)
     for i, (img_fn, mask_fn) in enumerate(zip(imgs, masks)):
+        _, base, _ = io.split_filename(img_fn)
         img = io.open_nii(img_fn)
-        mask = io.open_nii(mask_fn)
+        mask = io.open_nii(mask_fn) if mask_fn is not None else None
         if do_whitestripe:
-            logger.info('Applying WhiteStripe to image {} ({:d}/{:d})'.format(img_fn, i + 1, len(imgs)))
+            logger.info('Applying WhiteStripe to image {} ({:d}/{:d})'.format(base, i + 1, len(imgs)))
             inds = whitestripe(img, contrast, mask, verbose=verbose)
             img = whitestripe_norm(img, inds)
         img_data = img.get_data()
         V[:,i] = img_data.flatten()
-        if control_mask is not None:
-            Vc[:,i] = img_data[cmask_data == 1].flatten()
+        if return_ctrl_matrix:
+            logger.info('Finding control voxels for image {} ({:d}/{:d})'.format(base, i + 1, len(imgs)))
+            ctrl_mask = csf.csf_mask(img, mask, csf_thresh=membership_thresh, mrf=smoothness)
+            if np.sum(ctrl_mask) == 0:
+                raise NormalizationError('No control voxels found for image ({}) at threshold ({})'
+                                         .format(base, membership_thresh))
+            elif np.sum(ctrl_mask) < 100:
+                logger.warning('Few control voxels found ({:d}) (potentially a problematic image ({}) or '
+                               'threshold ({}) too high)'.format(np.sum(ctrl_mask), base, membership_thresh))
+            ctrl_vox.append(img_data[ctrl_mask == 1].flatten())
 
-    return V if control_mask is None else (V, Vc)
+    if return_ctrl_matrix:
+        min_len = min(map(len, ctrl_vox))
+        logger.info('Using {:d} control voxels'.format(min_len))
+        Vc = np.zeros((min_len, len(imgs)))
+        for i in range(len(imgs)):
+            ctrl_voxs = ctrl_vox[i][:min_len]
+            logger.info('Image {:d} control voxel stats -  mean: {:.3f}, std: {:.3f}'
+                         .format(i+1, np.mean(ctrl_voxs), np.std(ctrl_voxs)))
+            Vc[:,i] = ctrl_voxs
+
+    return V if not return_ctrl_matrix else (V, Vc)
 
 
 def image_matrix_to_images(V, imgs):
