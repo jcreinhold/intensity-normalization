c67a40a42150b8b65f406a5985ef1acffba56f67
Jacob Reinhold <jacob.reinhold@jhu.edu>
2020-03-22 16:18:29 -0400

    add save transform ability to coregister

--- intensity_normalization/exec/coregister.py
+++ intensity_normalization/exec/coregister.py
@@ -11,8 +11,10 @@ Created on: Jun 19, 2018
 """
 
 import argparse
+from glob import glob
 import logging
 import os
+import shutil
 import sys
 import warnings
 
@@ -34,6 +36,8 @@ def arg_parser():
     options = parser.add_argument_group('Options')
     options.add_argument('-t', '--template-dir', type=str, default=None,
                         help='directory of images to co-register the images to (if not provided, coreg to MNI)')
+    options.add_argument('-tfm', '--tfm-dir', type=str, default=None,
+                         help='directory of transformation files to register the images to (if not provided, will create tfm)')
     options.add_argument('--orientation', type=str, default='RAI',
                         help='output orientation of imgs')
     options.add_argument('-r', '--registration', type=str, default='Affine',
@@ -60,6 +64,7 @@ def main(args=None):
         if not os.path.exists(args.output_dir):
             logger.info('Making Output Directory: {}'.format(args.output_dir))
             os.mkdir(args.output_dir)
+            if args.tfm_dir is None: os.mkdir(os.path.join(args.output_dir, 'tfms'))
         if args.template_dir is None:
             logger.info('Registering image to MNI template')
             template = ants.image_read(ants.get_ants_data('mni')).reorient_image2(args.orientation)
@@ -69,6 +74,11 @@ def main(args=None):
             if len(template_fns) != len(img_fns):
                 raise NormalizationError('If template images are provided, they must be in '
                                          'correspondence (i.e., equal number) with the source images')
+        if args.tfm_dir is not None:
+            tfm_fns = sorted(glob(os.path.join(args.tfm_dir, '*.mat')))
+            if len(tfm_fns) != len(img_fns):
+                raise NormalizationError('If transforms are provided, they must be in '
+                                         'correspondence (i.e., equal number) with the source images')
         for i, img in enumerate(img_fns):
             _, base, _ = split_filename(img)
             logger.info('Registering image to template: {} ({:d}/{:d})'.format(base, i+1, len(img_fns)))
@@ -77,18 +87,26 @@ def main(args=None):
                 orientation = template.orientation if hasattr(template, 'orientation') else None
             input_img = ants.image_read(img)
             input_img = input_img.reorient_image2(orientation) if orientation is not None else input_img
-            if not args.no_rigid:
-                logger.info('Starting rigid registration: {} ({:d}/{:d})'.format(base, i+1, len(img_fns)))
-                mytx = ants.registration(fixed=template, moving=input_img, type_of_transform="Rigid")
-                tx = mytx['fwdtransforms'][0]
+            if args.tfm_dir is None:
+                if not args.no_rigid:
+                    logger.info('Starting rigid registration: {} ({:d}/{:d})'.format(base, i+1, len(img_fns)))
+                    mytx = ants.registration(fixed=template, moving=input_img, type_of_transform="Rigid")
+                    tx = mytx['fwdtransforms'][0]
+                else:
+                    tx = None
+                logger.info('Starting {} registration: {} ({:d}/{:d})'.format(args.registration, base, i+1, len(img_fns)))
+                mytx = ants.registration(fixed=template, moving=input_img, initial_transform=tx, type_of_transform=args.registration)
             else:
-                tx = None
-            logger.info('Starting {} registration: {} ({:d}/{:d})'.format(args.registration, base, i+1, len(img_fns)))
-            mytx = ants.registration(fixed=template, moving=input_img, initial_transform=tx, type_of_transform=args.registration)
+                logger.info('Loading transform: {} ({:d}/{:d})'.format(tfm_fns[i], i+1, len(img_fns)))
+                mytx = {'fwdtransforms': [tfm_fns[i]]}
             logger.debug(mytx)
             moved = ants.apply_transforms(template, input_img, mytx['fwdtransforms'], interpolator='bSpline')
             registered = os.path.join(args.output_dir, base + '_reg.nii.gz')
             ants.image_write(moved, registered)
+            if args.tfm_dir is None:
+                for j, tfm in enumerate(mytx['fwdtransforms']):
+                    tfm_fn = os.path.join(args.output_dir, 'tfms', base + '_{}.mat'.format(j))
+                    shutil.copy2(tfm, tfm_fn)
         return 0
     except Exception as e:
         logger.exception(e)
