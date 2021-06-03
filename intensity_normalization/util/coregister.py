fd5c19e0f6e0a7ee62b9022f20561be132dfa5d2
Jacob Reinhold <jcreinhold@gmail.com>
2021-06-03 16:35:53 -0400

    WIP: add coregister

---
+++ intensity_normalization/util/coregister.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+intensity-normalization.util.coregister
+
+Author: Jacob Reinhold (jcreinhold@gmail.com)
+Created on: Jun 03, 2021
+"""
+
+__all__ = []
+
+from typing import List, Optional
+
+import ants
+
+from intensity_normalization.type import NiftiImage
+
+
+class Registrator:
+    def __init__(
+        self,
+        template: Optional[NiftiImage] = None,
+        type_of_transform: str = "Affine",
+        interpolator: str = "bSpline",
+        initial_rigid: bool = True,
+    ):
+        if template is None:
+            standard_mni = ants.get_ants_data("mni")
+            self.template = ants.image_read(standard_mni)
+        else:
+            self.template = ants.from_nibabel(template)
+        self.type_of_transform = type_of_transform
+        self.interpolator = interpolator
+        self.initial_rigid = initial_rigid
+
+    def __call__(self, image: NiftiImage) -> NiftiImage:
+        return self.register(image)
+
+    def register(self, image: NiftiImage) -> NiftiImage:
+        image = ants.from_nibabel(image)
+        if self.initial_rigid:
+            transforms = ants.registration(
+                fixed=self.template, moving=image, type_of_transform="Rigid",
+            )
+            rigid_transform = transforms["fwdtransforms"][0]
+        else:
+            rigid_transform = None
+        transform = ants.registration(
+            fixed=self.template,
+            moving=image,
+            initial_transform=rigid_transform,
+            type_of_transform=self.type_of_transform,
+        )["fwdtransforms"]
+        registered = ants.apply_transforms(
+            self.template, image, transform, interpolator=self.interpolator,
+        )
+        return registered.to_nibabel()
+
+    def register_images(self, images: List[NiftiImage]) -> List[NiftiImage]:
+        return [self(image) for image in images]
+
+    def register_images_to_templates(
+        self, images: List[NiftiImage], templates: List[NiftiImage],
+    ) -> List[NiftiImage]:
+        registered = []
+        original_template = self.template
+        for image, template in zip(images, templates):
+            self.template = template
+            registered.append(self(image))
+        self.template = original_template
+        return registered
