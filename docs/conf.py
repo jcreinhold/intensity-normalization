4a4ac34a53cc2c2a0d52d0d2f8a4a2b8e802db1a
Jacob Reinhold <5241441+jcreinhold@users.noreply.github.com>
2021-08-18 13:05:54 -0400

    add doc configurations

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ html_theme = "alabaster"
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"sidebar_width": "250px"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
