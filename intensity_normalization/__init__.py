2d4bc17b8a1bc85880c8af32127a716ce6a9c685
Jacob Reinhold <5241441+jcreinhold@users.noreply.github.com>
2022-01-03 12:07:19 -0500

    decrease scanning frequency of codeql

--- intensity_normalization/__init__.py
+++ intensity_normalization/__init__.py
@@ -1,8 +1,15 @@
 """Top-level package for intensity-normalization."""
 
+import logging
+
+__title__ = "intensity-normalization"
+__description__ = "normalize the intensities of various MR image modalities"
+__url__ = "https://github.com/jcreinhold/intensity-normalization"
 __author__ = """Jacob Reinhold"""
 __email__ = "jcreinhold@gmail.com"
 __version__ = "2.1.2"
+__license__ = "Apache-2.0"
+__copyright__ = "Copyright 2021 Jacob Reinhold"
 
 PEAK = {
     "last": ["t1", "other", "last"],
@@ -11,3 +18,5 @@ PEAK = {
 }
 VALID_PEAKS = {m for modalities in PEAK.values() for m in modalities}
 VALID_MODALITIES = VALID_PEAKS - {"last", "largest", "first"}
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
