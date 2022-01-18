f5d724fa63ce6bd93e6808652404142044fe500f
Jacob Reinhold <jcreinhold@gmail.com>
2022-01-17 23:21:35 -0500

    formatting and import cleanups

--- intensity_normalization/cli/preprocess.py
+++ intensity_normalization/cli/preprocess.py
@@ -8,13 +8,8 @@ Created on: 13 Oct 2021
 
 __all__ = ["preprocess_main", "preprocess_parser"]
 
-try:
-    import ants
-except ImportError:
-    print("ANTsPy not installed. Install antspyx to use preprocess.")
-else:
-    from intensity_normalization.util.preprocess import Preprocessor
+from intensity_normalization.util.preprocess import Preprocessor
 
-    # main functions and parsers for CLI
-    preprocess_parser = Preprocessor.parser()
-    preprocess_main = Preprocessor.main(preprocess_parser)
+# main functions and parsers for CLI
+preprocess_parser = Preprocessor.parser()
+preprocess_main = Preprocessor.main(preprocess_parser)
--- intensity_normalization/cli/ravel.py
+++ intensity_normalization/cli/ravel.py
@@ -8,13 +8,8 @@ Created on: 13 Oct 2021
 
 __all__ = ["ravel_main", "ravel_parser"]
 
-try:
-    import ants
-except ImportError:
-    print("ANTsPy not installed. Install antspyx to use ravel-normalize.")
-else:
-    from intensity_normalization.normalize.ravel import RavelNormalize
+from intensity_normalization.normalize.ravel import RavelNormalize
 
-    # main functions and parsers for CLI
-    ravel_parser = RavelNormalize.parser()
-    ravel_main = RavelNormalize.main(ravel_parser)
+# main functions and parsers for CLI
+ravel_parser = RavelNormalize.parser()
+ravel_main = RavelNormalize.main(ravel_parser)
